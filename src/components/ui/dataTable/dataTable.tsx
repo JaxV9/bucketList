c661f5134925d35e792cc3236908e7c568c1c2fa
JaxV9 <layanj.pro@gmail.com>
2024-06-25 22:00:45 +0200

    working on habits

--- src/components/ui/dataTable/dataTable.tsx
+++ src/components/ui/dataTable/dataTable.tsx
@@ -8,7 +8,40 @@ export const DataTable = ({dataProps}:DataTablePropsType) => {
     return(
         <>
             <div className="dataTableContainer">
-
+                <div className="headDataTable">
+                    
+                    <span>Tâches</span>
+                </div>
+                <div className="dataTable">
+                    <span>Task 1</span>
+                </div>
+                <div className="dataTable">
+                    <span>Task 2</span>
+                </div>
+                <div className="dataTable">
+                    <span>Task 3</span>
+                </div>
+                <div className="dataTable">
+                    <span>Task 4</span>
+                </div>
+                <div className="dataTable">
+                    <span>Task 5</span>
+                </div>
+                <div className="dataTable">
+                    <span>Task 6</span>
+                </div>
+                <div className="dataTable">
+                    <span>Task 7</span>
+                </div>
+                <div className="dataTable">
+                    <span>Task 8</span>
+                </div>
+                <div className="dataTable">
+                    <span>Task 9</span>
+                </div>
+                <div className="dataTable">
+                    <span>Task 10</span>
+                </div>
             </div>
         </>
     )
